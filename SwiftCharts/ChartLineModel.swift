9f343f3bb61ed62491531411ad58e6cab7317526
Colin Reisterer <creisterer@projectdecibel.com>
2015-12-08 17:12:36 -0500

    Add Gradient support to drawing chart lines views

--- SwiftCharts/ChartLineModel.swift
+++ SwiftCharts/ChartLineModel.swift
@@ -9,23 +9,37 @@
 import UIKit
 
 public struct ChartLineModel<T: ChartPoint> {
-   
+
     let chartPoints: [T]
     let lineColor: UIColor
     let lineWidth: CGFloat
     let animDuration: Float
     let animDelay: Float
-    
-    public init(chartPoints: [T], lineColor: UIColor, lineWidth: CGFloat = 1, animDuration: Float, animDelay: Float) {
+    let gradient: GradientModel?
+
+    public init(chartPoints: [T], lineColor: UIColor, lineWidth: CGFloat = 1, animDuration: Float, animDelay: Float, gradient: GradientModel? = nil) {
         self.chartPoints = chartPoints
         self.lineColor = lineColor
         self.lineWidth = lineWidth
         self.animDuration = animDuration
         self.animDelay = animDelay
+        self.gradient = gradient
     }
-    
+
     var chartPointsCount: Int {
         return self.chartPoints.count
     }
-    
+
+}
+
+public struct GradientModel {
+    let colors: [UIColor]
+    let start: CGPoint
+    let end: CGPoint
+
+    public init(colors: [UIColor], start: CGPoint, end: CGPoint) {
+        self.colors = colors
+        self.start = start
+        self.end = end
+    }
 }
--- SwiftCharts/Views/ChartLinesView.swift
+++ SwiftCharts/Views/ChartLinesView.swift
@@ -19,13 +19,38 @@ public class ChartLinesView: UIView {
     private let animDuration: Float
     private let animDelay: Float
 
+    private let lineColors: [UIColor]?
+    private let gradientStart: CGPoint?
+    private let gradientEnd: CGPoint?
+
     init(path: UIBezierPath, frame: CGRect, lineColor: UIColor, lineWidth: CGFloat, animDuration: Float, animDelay: Float) {
-        
+
         self.lineColor = lineColor
         self.lineWidth = lineWidth
         self.animDuration = animDuration
         self.animDelay = animDelay
-        
+
+        self.lineColors = nil
+        self.gradientStart = nil
+        self.gradientEnd = nil
+
+        super.init(frame: frame)
+
+        self.backgroundColor = UIColor.clearColor()
+        self.show(path: path)
+    }
+
+    init(path: UIBezierPath, frame: CGRect, lineColors: [UIColor], gradientStart: CGPoint, gradientEnd: CGPoint, lineWidth: CGFloat, animDuration: Float, animDelay: Float) {
+
+        self.lineColor = lineColors.first ?? UIColor.blackColor()
+        self.lineWidth = lineWidth
+        self.animDuration = animDuration
+        self.animDelay = animDelay
+
+        self.lineColors = lineColors
+        self.gradientStart = gradientStart
+        self.gradientEnd = gradientEnd
+
         super.init(frame: frame)
 
         self.backgroundColor = UIColor.clearColor()
@@ -35,16 +60,16 @@ public class ChartLinesView: UIView {
     required public init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func createLineMask(frame frame: CGRect) -> CALayer {
         let lineMaskLayer = CAShapeLayer()
         var maskRect = frame
         maskRect.origin.y = 0
         maskRect.size.height = frame.size.height
         let path = CGPathCreateWithRect(maskRect, nil)
-        
+
         lineMaskLayer.path = path
-        
+
         return lineMaskLayer
     }
 
@@ -53,10 +78,10 @@ public class ChartLinesView: UIView {
         lineLayer.lineJoin = kCALineJoinBevel
         lineLayer.fillColor   = UIColor.clearColor().CGColor
         lineLayer.lineWidth   = self.lineWidth
-        
+
         lineLayer.path = path.CGPath;
         lineLayer.strokeColor = self.lineColor.CGColor;
-        
+
         if self.animDuration > 0 {
             lineLayer.strokeEnd   = 0.0
             let pathAnimation = CABasicAnimation(keyPath: "strokeEnd")
@@ -67,20 +92,40 @@ public class ChartLinesView: UIView {
             pathAnimation.autoreverses = false
             pathAnimation.removedOnCompletion = false
             pathAnimation.fillMode = kCAFillModeForwards
-            
+
             pathAnimation.beginTime = CACurrentMediaTime() + CFTimeInterval(self.animDelay)
             lineLayer.addAnimation(pathAnimation, forKey: "strokeEndAnimation")
-            
+
         } else {
             lineLayer.strokeEnd = 1
         }
-        
+
         return lineLayer
     }
-    
+
+    private func generateGradient() -> CAGradientLayer? {
+        guard let _lineColors = lineColors, _gradientStart = gradientStart, _gradientEnd = gradientEnd else {
+            return nil
+        }
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.startPoint = _gradientStart
+        gradientLayer.endPoint = _gradientEnd
+        gradientLayer.frame = CGRectMake(0, 0, layer.bounds.size.width, layer.bounds.size.height)
+        let cgcolors = _lineColors.map { $0.CGColor }
+        gradientLayer.colors = cgcolors
+        return gradientLayer
+    }
+
     private func show(path path: UIBezierPath) {
         let lineMask = self.createLineMask(frame: frame)
         self.layer.mask = lineMask
-        self.layer.addSublayer(self.generateLayer(path: path))
+        let lineLayer = generateLayer(path: path)
+        if let _gradient = generateGradient() {
+            _gradient.mask = lineLayer
+            layer.addSublayer(_gradient)
+        } else {
+            layer.addSublayer(lineLayer)
+        }
     }
- }
+}
