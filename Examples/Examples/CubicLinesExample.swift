609f3b99e0bd6dfe2b21a2e77c82934ad5513a1a
Colin Reisterer <creisterer@projectdecibel.com>
2015-12-08 17:19:20 -0500

    Add gradients to examples

--- Examples/Examples/CubicLinesExample.swift
+++ Examples/Examples/CubicLinesExample.swift
@@ -10,31 +10,42 @@ import UIKit
 import SwiftCharts
 
 class CubicLinesExample: UIViewController {
-    
+
     private var chart: Chart? // arc
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let labelSettings = ChartLabelSettings(font: ExamplesDefaults.labelFont)
 
         let chartPoints = [(0, 0), (4, 4), (8, 11), (9, 2), (11, 10), (12, 3), (15, 18), (18, 10), (20, 15)].map{ChartPoint(x: ChartAxisValueInt($0.0, labelSettings: labelSettings), y: ChartAxisValueInt($0.1))}
-        
+
         let xValues = chartPoints.map{$0.x}
         let yValues = ChartAxisValuesGenerator.generateYAxisValuesWithChartPoints(chartPoints, minSegmentCount: 10, maxSegmentCount: 20, multiple: 2, axisValueGenerator: {ChartAxisValueDouble($0, labelSettings: labelSettings)}, addPaddingSegmentIfEdge: false)
-        
+
         let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "Axis title", settings: labelSettings))
         let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "Axis title", settings: labelSettings.defaultVertical()))
         let chartFrame = ExamplesDefaults.chartFrame(self.view.bounds)
         let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: ExamplesDefaults.chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
         let (xAxis, yAxis, innerFrame) = (coordsSpace.xAxis, coordsSpace.yAxis, coordsSpace.chartInnerFrame)
-        
+
+
+        var colors = Array<UIColor>()
+        for var i: CGFloat = 0; i < 10; i += 1 {
+            let c = UIColor(hue: 0.1 * i, saturation: 1, brightness: 0.8, alpha: 0.6)
+            colors.append(c)
+        }
+        let p0 = CGPoint(x: 0.5, y: 0.0)
+        let p1 = CGPoint(x: 0.5, y: 1.0)
+
         let lineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor.purpleColor(), lineWidth: 2, animDuration: 1, animDelay: 0)
-        let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [lineModel], pathGenerator: CubicLinePathGenerator(tension1: 0.3, tension2: 0.3))
-        
+        let gradientModel = GradientModel(colors: colors, start: p0, end: p1)
+        let gradientLineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor.blackColor(), lineWidth: 2, animDuration: 1, animDelay: 0, gradient: gradientModel)
+        let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [gradientLineModel], pathGenerator: CubicLinePathGenerator(tension1: 0.3, tension2: 0.3))
+
         let settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.blackColor(), linesWidth: ExamplesDefaults.guidelinesWidth)
         let guidelinesLayer = ChartGuideLinesDottedLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, settings: settings)
-        
+
         let chart = Chart(
             frame: chartFrame,
             layers: [
@@ -44,7 +55,7 @@ class CubicLinesExample: UIViewController {
                 chartPointsLineLayer
             ]
         )
-        
+
         self.view.addSubview(chart.view)
         self.chart = chart
     }
